fa6ee29ff242ca094cb1db9f041ff91021b3e7ce
EclipseCantCode <logan.doug.mac@gmail.com>
2021-04-16 00:36:16 +1000

    kg kibuybiuly

--- lua/server.lua
+++ lua/server.lua
@@ -242,7 +242,7 @@ timer.Create("DoFuckedCuntDamage", 5, -1, function()
 	for k, v in pairs(cachedPlayers) do
 		local distanceToOrigin = (20000 - ((CurTime() - originTime) * 100))
 		local distanceToCompare = distanceToOrigin < 2000 and 2000 or distanceToOrigin
-
+		print("niger",v, distanceToCompare, k:GetPos():DistToSqr(origin))
 		if k:GetPos():DistToSqr(origin) < distanceToCompare then
 			k:TakeDamage(20, worldSpawn, worldSpawn)
 		end
