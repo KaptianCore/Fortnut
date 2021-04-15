95167269c795550fab9757a9bc1ef38d12f597b1
EclipseCantCode <logan.doug.mac@gmail.com>
2021-04-15 22:17:33 +1000

    Fuck ian lmao

--- lua/client.lua
+++ lua/client.lua
@@ -1,1 +1,13 @@
-chat.Add("Loaded Clientside fortnut")
+local origin, originTime
+
+net.Receive("eclipse.SendOrigin", function()
+	originTime, origin = net.ReadFloat(), net.ReadVector()
+end)
+
+hook.Add("PostDrawTranslucentRenderables", "DrawFuckedCunts", function()
+	if GetGlobalBool("gamestart") then
+		render.DrawWireframeSphere(origin, 20000 - ((CurTime() - originTime) * 100), 50, 50)
+	end
+end)
+
+chat.AddText("Loaded Clientside fortnut")
