05b3af8d8f5c2781b3814a68f85a375769ee91f0
EclipseCantCode <logan.doug.mac@gmail.com>
2021-04-16 13:57:29 +1000

    asdasd

--- lua/client.lua
+++ lua/client.lua
@@ -1,21 +1,25 @@
+local net_ReadEntity = net.ReadEntity
+local table_insert = table.insert
+local render_SetColorMaterial = CLIENT and render.SetColorMaterial
+local render_DrawSphere = render_DrawSphere
+local ipairs = ipairs
+local table_remove = table.remove
+local halo_Add = CLIENT and halo.Add
 local net_Receive = net.Receive
 local net_ReadFloat = net.ReadFloat
 local net_ReadVector = net.ReadVector
 local hook_Add = hook.Add
 local GetGlobalBool = GetGlobalBool
 local CurTime = CurTime
-local render_DrawWireframeSphere = CLIENT and render.DrawWireframeSphere
 local Color = Color
 local chat_AddText = CLIENT and chat.AddText
-local net_ReadEntity = net.ReadEntity
 origin = origin or nil
 originTime = originTime or nil
 local airdrops = {}
 
 net_Receive("eclipse.SendAirdrop", function()
-	local thing = net.ReadEntity()
-	print(thing)
-	table.insert(airdrops, thing)
+	local thing = net_ReadEntity()
+	table_insert(airdrops, thing)
 end)
 
 net_Receive("eclipse.SendOrigin", function()
@@ -26,7 +30,7 @@ hook_Add("PostDrawTranslucentRenderables", "DrawFuckedCunts", function()
 	if GetGlobalBool("gamestart") and origin and originTime then
 		local distanceToOrigin = (20000 - ((CurTime() - originTime) * 100))
 		local distanceToCompare = distanceToOrigin < 2000 and 2000 or distanceToOrigin
-		render.SetColorMaterial()
+		render_SetColorMaterial()
 		render_DrawSphere(origin, distanceToCompare, 50, 50, Color(0, 58, 14))
 	end
 end)
@@ -36,11 +40,11 @@ local color_green = Color(0, 255, 0)
 hook_Add("PreDrawHalos", "DrawIdiots", function()
 	for k, v in ipairs(airdrops) do
 		if not v:IsValid() then
-			table.remove(airdrops, k)
+			table_remove(airdrops, k)
 		end
 	end
 
-	halo.Add(airdrops, color_green, 5, 5, 1, true, true)
+	halo_Add(airdrops, color_green, 5, 5, 1, true, true)
 end)
 
 chat_AddText("Loaded Clientside fortnut")
