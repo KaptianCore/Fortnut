53b02b34786f1affe5025c307c73cb8063df836f
Kaptian Core <kaptiancore@gmail.com>
2021-07-26 16:08:30 +1000

    Update server.lua

--- lua/server.lua
+++ lua/server.lua
@@ -455,6 +455,7 @@ timer.Create("DoFuckedCuntDamage", 3, -1, function()
 end)
 
 hook.Add("PlayerDeath", "CuntDiedLmao", function(ply, inflictor, attacker)
+    if GetGlobalBool("gamestart") == false then return end
     cachedPlayers[ply] = nil
     if attacker:IsValid() and attacker:IsPlayer() then
         attacker:AddFrags(2)
